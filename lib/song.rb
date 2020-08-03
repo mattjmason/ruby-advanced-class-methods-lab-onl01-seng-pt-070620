c4216901b080f25a6610484799d969cddbbd6230
Matt Mason <masonmj10@gmail.com>
2020-08-03 04:06:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,15 +6,23 @@ class Song
     @@all
   end
   
+  def save
+    self.class.all << self
+  end
+  
 def self.create(name)
   song = self.new 
   song.save
   song 
 end
 
-  def save
-    self.class.all << self
-  end
+def self.new_by_name(name)
+  song = self.new 
+  song.name = name
+  song 
+end 
+
+  
 
 end
 
