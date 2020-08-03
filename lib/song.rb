aa11f6ff2db7879fe774604269c59272e866629a
Matt Mason <masonmj10@gmail.com>
2020-08-03 04:17:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,33 @@ def self.new_by_name(name)
   song 
 end 
 
+def self.find_by_name(name)
+  @@all.each do |song|
+    if song.name == name 
+      return song 
+    end
+  end
+end
+
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name
+end 
+
+def self.alphabetical
+  
+  @@all.sort_by{|song| song.name}
+end 
+
+def self.new_from_filename(filename)
+
+  row = filename  
+  
+  data = row.split(" - ")
+  artist_name = data [0]
+  song_name = data[1].gsub(".mp3", "")
+  
+  song = self.new 
+  
   
 
 end
